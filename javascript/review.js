97588672abe66336df84021d50d1a4b6e2bb71fa
Syed Nabi Hassan <syednabi932@gmail.com>
2020-10-04 11:33:15 +0500

    first commit

---
+++ javascript/review.js
@@ -0,0 +1,29 @@
+function searchcompany() {
+   var s = document.getElementById('add').value;
+   document.getElementById("company").innerHTML = s;
+}
+
+function gotohome(){
+    location.replace("home.html")
+}
+function gotoreview(){
+    location.replace("review.html")
+}
+function gotobrowsecategories(){
+    location.replace("browsecategories.html")
+}
+function gotoblog(){
+    location.replace("blog.html")
+}
+function gotologin(){
+    location.replace("login.html")
+}
+function gotosignup(){
+    location.replace("signup.html")
+}
+function gotopro(){
+    location.replace("pro.html")
+}
+function gotoforpass(){
+    location.replace("forpass.html")
+}
